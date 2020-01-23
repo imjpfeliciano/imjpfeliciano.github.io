b951044c7089f6814f18ecaca059f1a6a131389e
Jonathan Peraza Feliciano <jperazafeliciano@gmail.com>
2020-01-22 19:09:57 -0600

    chore: add dev command and new folder for landing design

---
+++ src/components/2020/LandingPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="landing-page">
+    <!-- <h1 class="title">imjpfeliciano</h1> -->
+    <h2 class="subtitle">Hi, I'm Jonathan, and I'm a Software Engineer</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+.landing-page {
+  background-color: $primary-color;
+  height: 100vh;
+
+  .title {
+    color: $primary-color-text;
+  }
+
+  .subtitle {
+    color: $primary-color-text;
+  }
+}
+</style>
