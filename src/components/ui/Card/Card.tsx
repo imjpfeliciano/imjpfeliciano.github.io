f89efd6edf7b3257622afbf95f07761900e1c6b6
Jonathan Peraza <jonathan_peraza@humanapi.co>
2022-07-07 08:33:00 -0500

    fix: card props children

--- src/components/ui/Card/Card.tsx
+++ src/components/ui/Card/Card.tsx
@@ -9,7 +9,11 @@ const CardContainer = styled.div`
 
 `;
 
-const Card = ({ children }) => {
+interface CardProps {
+    children: React.ReactNode;
+}
+
+const Card: React.FC<CardProps> = ({ children }) => {
     return (
         <CardContainer>{children}</CardContainer>
     )
