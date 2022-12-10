6dbbfc000722c1ee39f63b473c033f7f4991dd92
Jonathan Peraza Feliciano <jperazafeliciano@gmail.com>
2022-12-09 21:12:32 -0600

    fix: input mobile full size

--- src/components/ui/InputField/InputField.tsx
+++ src/components/ui/InputField/InputField.tsx
@@ -1,28 +1,30 @@
 import styled from "styled-components";
 
 const StyledInput = styled.input`
-    border: 1px solid #e2e8f0;
-    padding: 0.5rem;
-    border-radius: 5px;
-    font-size: 1rem;
-    margin: 0.5rem 0;
-    margin-right: 0.5rem;
+  border: 1px solid #e2e8f0;
+  padding: 0.5rem;
+  border-radius: 5px;
+  font-size: 1rem;
+  margin: 0.5rem 0;
+  margin-right: 0.5rem;
 
-    &:last-of-type {
-        margin-right: 0;
-    }
+  &:last-of-type {
+    margin-right: 0;
+  }
+
+  @media (max-width: 768px) {
+    margin-right: 0;
+  }
 `;
 
 interface InputFieldProps {
-    type: 'text' | 'email' | 'password';
-    name: string;
-    placeholder: string;
-    required?: boolean;
+  type: "text" | "email" | "password";
+  name: string;
+  placeholder: string;
+  required?: boolean;
 }
 const InputField = (inputProps: InputFieldProps) => {
-    return (
-        <StyledInput {...inputProps} />
-    )
-}
+  return <StyledInput {...inputProps} />;
+};
 
 export default InputField;
