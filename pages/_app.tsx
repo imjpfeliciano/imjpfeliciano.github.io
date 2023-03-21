b2fe4298698944a95b6e7572bd99e0f7bb68892b
Jonathan Peraza Feliciano <jperazafeliciano@gmail.com>
2023-03-20 23:10:48 -0600

    feat: add particles bg

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,26 @@
 import type { AppProps } from "next/app";
 import styled from "styled-components";
 import "../styles/globals.css";
+import { useCallback } from "react";
+import Particles from "react-particles";
+import { loadFull } from "tsparticles";
+import particlesConfig from "../src/utils/particles";
 
 const AppContainer = styled.div``;
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const particlesInit = useCallback((main: any): any => {
+    loadFull(main);
+  }, []);
+
   return (
     <AppContainer>
+      <Particles
+        id="tsparticles"
+        options={particlesConfig}
+        init={particlesInit}
+        style={{ zIndex: -1 }}
+      />
       <Component {...pageProps} />
     </AppContainer>
   );
---
+++ src/utils/particles.ts
@@ -0,0 +1,70 @@
+const particlesConfig: any = {
+  background: {
+    color: "transparent",
+  },
+  interactivity: {
+    events: {
+      onClick: {
+        enable: true,
+        mode: "push",
+      },
+      onHover: {
+        enable: true,
+        mode: "repulse",
+      },
+      resize: true,
+    },
+    modes: {
+      push: {
+        quantity: 4,
+      },
+      repulse: {
+        distance: 100,
+        duration: 0.4,
+      },
+    },
+  },
+  particles: {
+    color: {
+      value: "#ffffff",
+    },
+    links: {
+      color: "#ffffff",
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    collisions: {
+      enable: true,
+    },
+    move: {
+      direction: "none",
+      enable: true,
+      outModes: {
+        default: "bounce",
+      },
+      random: false,
+      speed: 1,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+      },
+      value: 80,
+    },
+    opacity: {
+      value: 0.5,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      random: true,
+      value: 5,
+    },
+  },
+};
+
+export default particlesConfig;
