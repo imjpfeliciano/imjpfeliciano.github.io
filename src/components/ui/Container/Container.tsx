b69bfd8c65e638bd81721a6cdd109376afe4cfcd
Jonathan Peraza <jonathan_peraza@humanapi.co>
2023-06-06 14:31:26 -0600

    feat: add tailwind support

--- src/components/ui/Container/Container.tsx
+++ src/components/ui/Container/Container.tsx
@@ -5,9 +5,13 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
 
-  max-width: 80%;
+  max-width: 95%;
   margin: 0 auto;
 
+  @media (min-width: 768px) {
+    max-width: 80%;
+  }
+  
   @media (min-width: 1366px) {
     max-width: 50%;
   }
