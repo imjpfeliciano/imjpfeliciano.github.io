d14c20c9c2682f1519a4236a2bc817ca4ed13717
Jonathan Peraza <jonathan.peraza@cerby.com>
2023-10-01 09:56:32 -0500

    chore: add footer copy

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,20 @@ const currentYear = new Date().getFullYear();
 
 const Footer = () => {
     return (
-        <footer className='dark:text-white font-bold font-2xl flex flex-row items-center justify-center'>© Copyright {currentYear}. All Rights Reserved.</footer>
+        <footer className='dark:text-white font-bold font-2xl flex flex-col items-center justify-center pb-[4rem]'>
+            <span>© Copyright {currentYear}. All Rights Reserved.</span>
+            <span>
+                Made with <span className='text-red-500'>❤</span> by{" "}
+                <a
+                    href='https://github.com/imjpfeliciano'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    className='text-blue-700 dark:text-lime-400'
+                >
+                    Jonathan Peraza F
+                </a>
+            </span>
+        </footer>
     )
 }
 
