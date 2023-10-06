6d8d3c0f547468270bd3a16b2e78480f2a535f81
Jonathan Peraza <jonathan.peraza@cerby.com>
2023-10-06 12:54:52 -0500

    feat: update experience item card

--- src/components/ui/Timeline/TimelineItem.tsx
+++ src/components/ui/Timeline/TimelineItem.tsx
@@ -16,20 +16,21 @@ const TimelineItem: React.FC<TimelineItem> = ({
 }) => {
   return (
     <Card>
-      <div className="text-center">
-        <h3 className="text-slate-800 dark:text-slate-500 text-lg font-light">
-          {`${company} { ${date} }`}
-        </h3>
-        <h2 className="text-black dark:text-white font-bold text-xl">{title}</h2>
+      <div className="flex flex-row gap-4">
+        <div className="w-1/3">
+          <h3 className="text-slate-800 dark:text-slate-500 text-lg font-light">
+            {`${company} { ${date} }`}
+          </h3>
+          <h2 className="text-black dark:text-white font-bold text-xl">{title}</h2>
+        </div>
 
         <div
-          className="text-slate-600 dark:text-slate-400 text-base mt-2"
+          className="text-slate-600 dark:text-slate-400 text-base w-2/3"
           dangerouslySetInnerHTML={{
             __html: description,
           }}
         />
       </div>
-
     </Card>
   );
 };
